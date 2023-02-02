076510023f852bfb09085ff7dd66092d55d45fb9
Md. Ayub Ali <76879092+ayub1145@users.noreply.github.com>
2023-02-02 04:33:53 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+var typingEffect = new Typed(".multiText",{
+  strings:["Web Desinger","Coder","Youtuber"],
+  loop:true,
+  typeSpeed:100,
+  backSpeed:90,
+  startDelay:1000,
+  backDelay:1500,
+})
